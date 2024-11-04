266b0b9c4d2223d2920d784a00f3d5de3c58eef1
Rafa Gómez <rgomezcasas@gmail.com>
2024-11-04 18:01:31 +0100

    feat(5-1): add web scrapping with playwright

--- 05-3r_party_data/1-web_scrapping/src/app/scripts/scrape-courses.ts
+++ 05-3r_party_data/1-web_scrapping/src/app/scripts/scrape-courses.ts
@@ -1,21 +1,81 @@
 /* eslint-disable no-console */
 import "reflect-metadata";
 
-import { RecursiveUrlLoader } from "@langchain/community/document_loaders/web/recursive_url";
-import { compile } from "html-to-text";
+import { Document } from "@langchain/core/documents";
+import { chromium } from "playwright";
+
+// import { RecursiveUrlLoader } from "@langchain/community/document_loaders/web/recursive_url";
+// import { compile } from "html-to-text";
+//
+// async function main(): Promise<void> {
+// 	const url = "http://localhost:3012";
+// 	const compiledConvert = compile({ wordwrap: 130 });
+//
+// 	const loader = new RecursiveUrlLoader(url, {
+// 		extractor: compiledConvert,
+// 		maxDepth: 1,
+// 	});
+// 	const docs = await loader.load();
+// 	console.log(docs);
+// }
+
+async function scrapeCourse(url: string): Promise<Document> {
+	const browser = await chromium.launch({ headless: true });
+	const page = await browser.newPage();
+
+	try {
+		await page.goto(url);
+
+		const content = await page.evaluate(() => {
+			const description =
+				document.querySelector("[class^='course_course__description']")
+					?.textContent ?? "";
+
+			const steps = document.querySelectorAll<HTMLAnchorElement>(
+				"[class^='course_course__lessons'] a",
+			);
+
+			const formattedSteps = Array.from(steps)
+				.map((step) => {
+					return `* ${step.textContent}: ${step.href}`;
+				})
+				.join("\n");
+
+			return `
+Descripción
+---------------
+${description}
+
+Vídeos del curso
+---------------
+${formattedSteps}
+`.trim();
+		});
+
+		return new Document({ pageContent: content });
+	} finally {
+		await browser.close();
+	}
+}
 
 async function main(): Promise<void> {
-	const url = "http://localhost:3012";
-	const compiledConvert = compile({ wordwrap: 130 });
+	const urls = [
+		"http://localhost:3012/anade-inteligencia-artificial-siguiendo-buenas-practicas.html",
+		"http://localhost:3012/asincronia-en-javascript.html",
+		"http://localhost:3012/auditoria-holaluz.html",
+		"http://localhost:3012/ddd-microservicios-e-infra-en-audiense-genially-y-codely.html",
+		"http://localhost:3012/diseno-de-infraestructura-aws-sqs-como-cola-de-mensajeria.html",
+		"http://localhost:3012/diseno-de-infraestructura-rabbitmq-como-cola-de-mensajeria.html",
+		"http://localhost:3012/modelado-del-dominio-value-objects.html",
+		"http://localhost:3012/patrones-de-diseno-criteria.html",
+		"http://localhost:3012/tratamiento-de-datos-en-bash-gestiona-archivos-json-xml-yaml.html",
+	];
 
-	const loader = new RecursiveUrlLoader(url, {
-		extractor: compiledConvert,
-		maxDepth: 1,
-	});
-	const docs = await loader.load();
-
-	// raw HTML page content
-	// const extractedContents = docs[0].pageContent;
+	const docs = await Promise.all(
+		urls.map((url) => {
+			return scrapeCourse(url);
+		}),
+	);
 
 	console.log(docs);
 }
